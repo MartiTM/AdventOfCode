ff76a7a08a5e8feab76cd422ad077bccf60253f4
Thomas <thomas.2905@hotmail.fr>
2023-12-23 12:31:36 +0000

    2023 day 3 refacto + part2

--- 2023/day_3/main.go
+++ 2023/day_3/main.go
@@ -9,111 +9,104 @@ import (
 	"github.com/MartiTM/AdventOfCode2022/util"
 )
 
-func getCoordByLine(enginMap [][]byte, regex *regexp.Regexp) [][][]int {
-	result := [][][]int{}
-
-	for _, line := range enginMap {
-		result = append(result, regex.FindAllIndex(line, -1))
-	}
-
-	return result
+type Coord struct {
+	y int
+	x int
+	value string
 }
 
-func getNumberCloseToSymbolCoord(nbCoord [][][]int , symbolCoord [][][]int, neededMatch int) [][][]int {
-	result := [][][]int{}
-	for y, line := range nbCoord {
-		resultLine := [][]int{}
-		for _, coord := range line {
-			xStart := coord[0]
-			xEnd := coord[1] - 1
-			if isCloseFromSymbol(y, xStart, xEnd, symbolCoord, neededMatch) {
-				resultLine = append(resultLine, coord)
-			}
-		}
-		result = append(result, resultLine)
-	}
-	return result
+// Only work if we use engine part on base
+func (base Coord) isClose(target Coord) bool {
+	return base.y - 1 <= target.y && target.y <= base.y + 1 && base.x - 1 <= target.x && target.x <= (base.x + len(base.value))
 }
 
-func isCloseFromSymbol(numberY int , numberStartX int, numberEndX int, symbolCoord [][][]int, neededMatch int) bool {
-	startX := numberStartX - 1
-	endX := numberEndX + 1
+func main() {
+	data := util.GetRawData("./2023/day_3/inputTest")
+	engineMap := bytes.Split(data, []byte("\n"))
+
+	engineParts, symbols := getEngineAndSymbols(engineMap)
+
+	enginePartsClose := getEnginePartsCloseToSymbol(engineParts, symbols)
+
+	total := getTotalEngineParts(enginePartsClose)
+
+	fmt.Printf("%v\n", total)
 	
-	startY := numberY - 1
-	endY := numberY + 1
+	total = getTotalPart2(engineParts, symbols)
 
-	if startY < 0 {
-		startY = numberY
-	}
-	if endY >= len(symbolCoord) {
-		endY = len(symbolCoord) - 1
-	}
-
-	match := 0
-	
-	// fmt.Printf("Ligne %v : coord %v:%v\n", numberY, numberStartX, numberEndX)
-	for y := startY; y <= endY; y++ {
-		for _, symboleCoord := range symbolCoord[y] {
-			// fmt.Println(startX, " >= ", symboleCoord[0], " <= ", endX )
-			if startX <= symboleCoord[0] && symboleCoord[0] <= endX {
-				// fmt.Printf("trouvé sur %v\n", y)
-				match++
-			}
-		}
-	}
-
-	// fmt.Printf("pas trouvé de %v a %v \n", startY, endY)
-	return match >= neededMatch
+	fmt.Printf("%v\n", total)
 }
 
-func getTotal(enginMap [][]byte, numberCoord [][][]int) int {
+func getTotalPart2(engineParts []Coord, symbols []Coord) int {
 	total := 0
-	
-	for i, coords := range numberCoord {
-		for _, coord := range coords {
-			bArray := enginMap[i][coord[0]:coord[1]]
-			num, _ := strconv.Atoi(string(bArray))
-			total+= num
-		} 
+
+	for _, symbol := range symbols {
+		if symbol.value != "*" {
+			continue
+		}
+		matches := []Coord{}
+		for _, part := range engineParts {
+			if part.isClose(symbol) {
+				matches = append(matches, part)
+			}
+		}
+		if len(matches) == 2 {
+			num1, _ := strconv.Atoi(matches[0].value)
+			num2, _ := strconv.Atoi(matches[1].value)
+			total+= (num1*num2)
+		}
+	}
+	return total
+} 
+
+func getTotalEngineParts(enginePars []Coord) int {
+	total := 0
+
+	for _, part := range enginePars {
+		num, _ := strconv.Atoi(part.value)
+		total+= num
 	}
 
 	return total
 }
 
-func part1(){
-	data := util.GetRawData("./2023/day_3/input")
-	enginMap := bytes.Split(data, []byte("\n"))
+func getEnginePartsCloseToSymbol(engineParts []Coord, symbols []Coord) []Coord {
+	result := []Coord{}
+
+	for _, part := range engineParts {
+		for _, symbol := range symbols {
+			if part.isClose(symbol) {
+				result = append(result, part)
+				break
+			}
+		}
+	}
+
+	return result
+}
+
+func getEngineAndSymbols(engineMap [][]byte) ([]Coord, []Coord) {
 
 	regexGetNumber := regexp.MustCompile(`[0-9]+`)
-	numberCoord := getCoordByLine(enginMap, regexGetNumber)
+	engineParts := getCoord(engineMap, regexGetNumber)
 
 	regexGetSymbol := regexp.MustCompile(`[^0-9|a-zA-Z|.]`)
-	symbolCoord := getCoordByLine(enginMap, regexGetSymbol)
+	symbols := getCoord(engineMap, regexGetSymbol)
 
-	numberCloseToSymbolCoord := getNumberCloseToSymbolCoord(numberCoord, symbolCoord, 1)
-
-	total := getTotal(enginMap, numberCloseToSymbolCoord)
-
-	fmt.Printf("%v\n", total)
+	return engineParts, symbols
 }
 
-func part2() {
-	data := util.GetRawData("./2023/day_3/input")
-	enginMap := bytes.Split(data, []byte("\n"))
+func getCoord(engineMap [][]byte, regex *regexp.Regexp) []Coord {
+	result := []Coord{}
 
-	regexGetNumber := regexp.MustCompile(`[0-9]+`)
-	numberCoord := getCoordByLine(enginMap, regexGetNumber)
+	for y, line := range engineMap {
+		matchesIndex := regex.FindAllIndex(line, -1)
+		for _, index := range matchesIndex {
+			value := line[index[0]:index[1]]
+			coord := Coord{y, index[0], string(value)}
+			result = append(result, coord)
+		}
+	}
 
-	regexGetSymbol := regexp.MustCompile(`[*]`)
-	symbolCoord := getCoordByLine(enginMap, regexGetSymbol)
-
-	numberCloseToSymbolCoord := getNumberCloseToSymbolCoord(numberCoord, symbolCoord, 2)
-
-	total := getTotal(enginMap, numberCloseToSymbolCoord)
-
-	fmt.Printf("%v\n", total)
-}
-
-func main() {
-	part2()
+	return result
 }
